a9caa46ba6bf9ba686870c5c33e44eca70769a74
Yuto Maruta <maruta-yuto@g.ecc.u-tokyo.ac.jp>
2026-01-16 22:57:53 +0900

    fix: ignore directries from watch in development

--- vite.config.ts
+++ vite.config.ts
@@ -34,14 +34,14 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ["**/src-tauri/**", "**/target/**", "**/node_modules/**", "**/.git/**", "**/dist/**"],
     },
   },
 }))
