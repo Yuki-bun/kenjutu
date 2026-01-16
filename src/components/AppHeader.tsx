ddfa072f8b4e6759443bde2b3de5a90c63cc6177
Yuto Maruta <115913823+Yuki-bun@users.noreply.github.com>
2026-01-16 23:58:16 +0900

    feat: diff view ui improvements (#21)
    - initally collapse file only when the file is reviewd
    - collapse file when marked as reviewd
    - sticky file name
    - made navbar non-sticky

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -33,7 +33,7 @@ export function AppHeader() {
   const isAuthenticating = authMutation.isPending
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+    <header className="z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <nav className="container flex h-14 items-center justify-between px-4">
         <Link to={"/"}>
           <div className="font-semibold text-lg">PR Manager</div>
