eb6a3b18c037838848b5afa44ad292b6d51161d5
Yuto Maruta <maruta-yuto@g.ecc.u-tokyo.ac.jp>
2026-01-20 16:46:09 +0900

    fix: recompile only when GITHUB_APP_CLIENT_SECRET changes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let _ = dotenvy::dotenv();
 
     // tell rust to recompile when .env changes
-    println!("cargo:rerun-if-changed=.env");
+    println!("cargo:rerun-if-env-changed=GITHUB_APP_CLIENT_SECRET");
 
     if let Ok(val) = std::env::var("GITHUB_APP_CLIENT_SECRET") {
         println!("cargo:rustc-env=GITHUB_APP_CLIENT_SECRET={}", val);
