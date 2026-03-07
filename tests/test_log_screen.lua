814b565695c2f06613f4dca34a7cbfa4f98aa022
Yuto Maruta <myu7253@gmail.com>
2026-03-07 17:18:31 +0900

    test(nvim): add Tier 2 Neovim API integration tests
    Log screen lifecycle (tab creation, filetype, sidebar, keymaps, close) and
    review screen (three-pane layout, file list rendering, keymaps, close
    restores log). Uses synchronous mocks for kjn/jj modules.

---
+++ tests/test_log_screen.lua
@@ -0,0 +1,185 @@
+---@diagnostic disable: duplicate-set-field
+local t = require("tests.test")
+
+local jj = require("kenjutu.jj")
+local kjn = require("kenjutu.kjn")
+
+local original_jj_log = jj.log
+local original_jj_fetch_metadata = jj.fetch_commit_metadata
+local original_kjn_run = kjn.run
+
+local mock_log_result = {
+  lines = {
+    "o  abc123 user commit one",
+    "  first commit message",
+    "o  def456 user commit two",
+    "  second commit message",
+  },
+  highlights = {},
+  commits_by_line = {
+    [1] = { change_id = "aaaa1111", commit_id = "cccc1111" },
+    [3] = { change_id = "bbbb2222", commit_id = "dddd2222" },
+  },
+  commit_lines = { 1, 3 },
+}
+
+local function install_mocks()
+  jj.log = function(_, callback)
+    callback(nil, mock_log_result)
+  end
+  jj.fetch_commit_metadata = function(_, _, callback)
+    callback(nil, { summary = "test", description = "", author = "test", timestamp = "1s ago" })
+  end
+  kjn.files = function(_, _, callback)
+    callback(nil, { files = {}, changeId = "abc123", commitId = "abc123" })
+  end
+end
+
+local function restore_mocks()
+  jj.log = original_jj_log
+  jj.fetch_commit_metadata = original_jj_fetch_metadata
+  kjn.run = original_kjn_run
+end
+
+local function cleanup_tabs()
+  while #vim.api.nvim_list_tabpages() > 1 do
+    vim.cmd("tabclose!")
+  end
+end
+
+---@param ft string
+---@return integer|nil bufnr
+---@return integer|nil winnr
+local function find_buf_by_ft(ft)
+  for _, w in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local b = vim.api.nvim_win_get_buf(w)
+    if vim.bo[b].filetype == ft then
+      return b, w
+    end
+  end
+  return nil, nil
+end
+
+local function log_case(name, fn)
+  t.run_case(name, function()
+    install_mocks()
+    local ok, err = pcall(fn)
+    restore_mocks()
+    cleanup_tabs()
+    if not ok then
+      error(err, 0)
+    end
+  end)
+end
+
+-- log screen ------------------------------------------------------------------
+
+log_case("open creates tab with correct layout", function()
+  local tabs_before = #vim.api.nvim_list_tabpages()
+  require("kenjutu.log").open()
+
+  t.eq(#vim.api.nvim_list_tabpages(), tabs_before + 1)
+  t.neq(find_buf_by_ft("kenjutu-log"), nil)
+  t.neq(find_buf_by_ft("kenjutu-log-files"), nil)
+  t.eq(#vim.api.nvim_tabpage_list_wins(0), 2)
+end)
+
+log_case("j moves cursor to next commit line", function()
+  require("kenjutu.log").open()
+  local _, winnr = find_buf_by_ft("kenjutu-log")
+  assert(winnr, "could not find log window")
+  vim.api.nvim_set_current_win(winnr)
+
+  vim.api.nvim_win_set_cursor(winnr, { 1, 0 })
+  vim.api.nvim_feedkeys("j", "x", false)
+  t.eq(vim.api.nvim_win_get_cursor(winnr)[1], 3, "j should jump to second commit line")
+
+  vim.api.nvim_feedkeys("j", "x", false)
+  t.eq(vim.api.nvim_win_get_cursor(winnr)[1], 3, "j at last commit should stay put")
+end)
+
+log_case("k moves cursor to previous commit line", function()
+  require("kenjutu.log").open()
+  local _, winnr = find_buf_by_ft("kenjutu-log")
+  assert(winnr, "could not find log window")
+  vim.api.nvim_set_current_win(winnr)
+
+  vim.api.nvim_win_set_cursor(winnr, { 3, 0 })
+  vim.api.nvim_feedkeys("k", "x", false)
+  t.eq(vim.api.nvim_win_get_cursor(winnr)[1], 1, "k should jump to first commit line")
+
+  vim.api.nvim_feedkeys("k", "x", false)
+  t.eq(vim.api.nvim_win_get_cursor(winnr)[1], 1, "k at first commit should stay put")
+end)
+
+log_case("<CR> opens review screen for commit under cursor", function()
+  require("kenjutu.log").open()
+  local _, winnr = find_buf_by_ft("kenjutu-log")
+  assert(winnr, "could not find log window")
+  vim.api.nvim_set_current_win(winnr)
+
+  vim.api.nvim_win_set_cursor(winnr, { 1, 0 })
+  vim.api.nvim_feedkeys("\r", "x", false)
+
+  t.eq(vim.bo.filetype, "kenjutu-review-files", "<CR> on commit line should open review screen")
+end)
+
+log_case("<CR> does nothing on non-commit line", function()
+  require("kenjutu.log").open()
+  local _, winnr = find_buf_by_ft("kenjutu-log")
+  assert(winnr, "could not find log window")
+  vim.api.nvim_set_current_win(winnr)
+
+  vim.api.nvim_win_set_cursor(winnr, { 2, 0 })
+  vim.api.nvim_feedkeys("\r", "x", false)
+
+  t.eq(vim.bo.filetype, "kenjutu-log", "<CR> on non-commit line should stay on log")
+end)
+
+log_case("r refreshes the log buffer content", function()
+  require("kenjutu.log").open()
+  local log_bufnr, winnr = find_buf_by_ft("kenjutu-log")
+  assert(log_bufnr and winnr, "could not find log buffer")
+  vim.api.nvim_set_current_win(winnr)
+
+  vim.api.nvim_win_set_cursor(winnr, { 3, 0 })
+
+  local updated_lines = {
+    "o  fff789 user commit three",
+    "  third commit message",
+    "o  abc123 user commit one",
+    "  first commit message",
+    "o  def456 user commit two",
+    "  second commit message",
+  }
+  jj.log = function(_, callback)
+    callback(nil, {
+      lines = updated_lines,
+      highlights = {},
+      commits_by_line = {
+        [1] = { change_id = "cccc3333", commit_id = "eeee3333" },
+        [3] = { change_id = "aaaa1111", commit_id = "cccc1111" },
+        [5] = { change_id = "bbbb2222", commit_id = "dddd2222" },
+      },
+      commit_lines = { 1, 3, 5 },
+    })
+  end
+
+  vim.api.nvim_feedkeys("r", "x", false)
+
+  local buf_lines = vim.api.nvim_buf_get_lines(log_bufnr, 0, -1, false)
+  t.eq(buf_lines[1], updated_lines[1], "buffer content should reflect refreshed data")
+  t.eq(vim.api.nvim_win_get_cursor(winnr)[1], 5, "cursor should follow the same commit after refresh")
+end)
+
+log_case("q closes the tab", function()
+  require("kenjutu.log").open()
+  local tabs_before = #vim.api.nvim_list_tabpages()
+
+  local _, winnr = find_buf_by_ft("kenjutu-log")
+  assert(winnr, "could not find log buffer window")
+  vim.api.nvim_set_current_win(winnr)
+  vim.api.nvim_feedkeys("q", "x", false)
+
+  t.eq(#vim.api.nvim_list_tabpages(), tabs_before - 1)
+end)
---
+++ tests/test_review.lua
@@ -0,0 +1,137 @@
+---@diagnostic disable: duplicate-set-field
+local t = require("tests.test")
+
+local kjn = require("kenjutu.kjn")
+local review = require("kenjutu.review")
+
+local original_kjn_run = kjn.run
+local original_kjn_run_raw = kjn.run_raw
+
+local mock_files = {
+  {
+    newPath = "src/main.lua",
+    oldPath = "src/main.lua",
+    status = "modified",
+    reviewStatus = "unreviewed",
+    additions = 5,
+    deletions = 2,
+    isBinary = false,
+  },
+  {
+    newPath = "src/utils.lua",
+    oldPath = "src/utils.lua",
+    status = "added",
+    reviewStatus = "reviewed",
+    additions = 10,
+    deletions = 0,
+    isBinary = false,
+  },
+}
+
+local function install_mocks()
+  kjn.files = function(_, change_id, cb)
+    cb(nil, {
+      files = mock_files,
+      commitId = "abc123",
+      changeId = change_id,
+    })
+  end
+  kjn.run_raw = function(_, _, callback)
+    callback(nil, "line1\nline2\nline3\n")
+  end
+end
+
+local function restore_mocks()
+  kjn.run = original_kjn_run
+  kjn.run_raw = original_kjn_run_raw
+end
+
+---@param ft string
+---@return integer|nil bufnr
+---@return integer|nil winnr
+local function find_buf_by_ft(ft)
+  for _, w in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local b = vim.api.nvim_win_get_buf(w)
+    if vim.bo[b].filetype == ft then
+      return b, w
+    end
+  end
+  return nil, nil
+end
+
+local function open_review()
+  local log_bufnr = vim.api.nvim_get_current_buf()
+  local commit = { change_id = "test_change", commit_id = "test_commit" }
+  review.open(vim.fn.getcwd(), commit, log_bufnr, function() end)
+  return log_bufnr
+end
+
+local function review_case(name, fn)
+  t.run_case(name, function()
+    install_mocks()
+    vim.cmd("tabnew")
+    local ok, err = pcall(fn)
+    restore_mocks()
+    while #vim.api.nvim_list_tabpages() > 1 do
+      vim.cmd("tabclose!")
+    end
+    if not ok then
+      error(err, 0)
+    end
+  end)
+end
+
+-- review screen ---------------------------------------------------------------
+
+review_case("creates three-pane layout", function()
+  open_review()
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+  t.eq(#wins, 3)
+end)
+
+review_case("file list buffer has correct filetype", function()
+  open_review()
+  local bufnr = find_buf_by_ft("kenjutu-review-files")
+  t.neq(bufnr, nil)
+end)
+
+review_case("file list keymaps are registered", function()
+  open_review()
+  local file_list_bufnr = find_buf_by_ft("kenjutu-review-files")
+  assert(file_list_bufnr, "file list buffer not found")
+
+  local keymaps = vim.api.nvim_buf_get_keymap(file_list_bufnr, "n")
+  local expected_keys = { "j", "k", "<CR>", " ", "r", "t", "q" }
+  for _, key in ipairs(expected_keys) do
+    local found = false
+    for _, km in ipairs(keymaps) do
+      if km.lhs == key then
+        found = true
+        break
+      end
+    end
+    t.ok(found, "expected keymap '" .. key .. "' to be registered")
+  end
+end)
+
+review_case("file list renders files correctly", function()
+  open_review()
+  local file_list_bufnr = find_buf_by_ft("kenjutu-review-files")
+  assert(file_list_bufnr, "file list buffer not found")
+
+  local lines = vim.api.nvim_buf_get_lines(file_list_bufnr, 0, -1, false)
+  t.eq(#lines, 4)
+  t.neq(lines[1]:find("Files 1/2"), nil)
+end)
+
+review_case("close restores log buffer", function()
+  local log_bufnr = open_review()
+
+  local _, winnr = find_buf_by_ft("kenjutu-review-files")
+  assert(winnr, "file list window not found")
+  vim.api.nvim_set_current_win(winnr)
+  vim.api.nvim_feedkeys("q", "x", false)
+
+  local cur_buf = vim.api.nvim_get_current_buf()
+  t.eq(cur_buf, log_bufnr)
+end)
